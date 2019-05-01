18644ef1297d29d3a1da23675376ad22f028a03e
Miktor <miktorkiraken@gmail.com>
2019-05-01 16:08:02 +0300

    Второй комит

--- JS_GAME/script.js
+++ JS_GAME/script.js
@@ -1,7 +1,39 @@
 'use strict';
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
 
-console.log(leftBorderWidth);
+// console.log(leftBorderWidth);
+
+// let options = {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
+
+// console.log(options.name)
+
+let money = +prompt("Ваш бюджет на месяц?($)", "150");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+let costs;
+let expensive;
+
+costs = prompt("Введите обязательную статью расходов в этом месяце", "Еда, коммунальные платежи")
+expensive = +prompt("Во сколько обойдется?($)", "50")
+
+
+let appData = {
+    moneyData: money,
+    timeData: time,
+    expenses: {
+        costs, 
+        expensive    },
+    optionalExpenses: undefined,
+    income: undefined,
+    savings: false
+};
+
+let moneyDay =(appData.moneyData - appData.expenses.expensive)/30;
+
+let message = alert(" Дата: " + appData.timeData + " С учетом расходов на:" + appData.expenses.costs + " Бюджет на 1 день составляет: " + Math.round(moneyDay) + "$");
