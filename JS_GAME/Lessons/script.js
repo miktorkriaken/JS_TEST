9bf1a9084cffd34b58bad4d9e342f33b7ab71f3f
Miktor <miktorkiraken@gmail.com>
2019-05-01 18:44:45 +0300

    commit 3

---
+++ JS_GAME/Lessons/script.js
@@ -0,0 +1,2 @@
+let x = 2 && 1 && null && 0 && undefined;
+console.log(x);
--- JS_GAME/script.js
+++ JS_GAME/script.js
@@ -1,39 +1,79 @@
 'use strict';
 
-// var leftBorderWidth = 1;
-// let second = 2;
-// const pi = 3.14;
+//      Version 1   //
 
-// console.log(leftBorderWidth);
+// let money = +prompt("Ваш бюджет на месяц?($)", "150");
+// let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+// let costs;
+// let expensive;
 
-// let options = {
-//     width: 1024,
-//     height: 1024,
-//     name: "test"
+// costs = prompt("Введите обязательную статью расходов в этом месяце", "Еда, коммунальные платежи")
+// expensive = +prompt("Во сколько обойдется?($)", "50")
+
+
+// let appData = {
+//     moneyData: money,
+//     timeData: time,
+//     expenses: {
+//         costs, 
+//         expensive    },
+//     optionalExpenses: undefined,
+//     income: undefined,
+//     savings: false
 // };
 
-// console.log(options.name)
+// let moneyDay =(appData.moneyData - appData.expenses.expensive)/30;
 
-let money = +prompt("Ваш бюджет на месяц?($)", "150");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
-let costs;
-let expensive;
+// let message = alert(" Дата: " + appData.timeData + " С учетом расходов на:" + appData.expenses.costs + " Бюджет на 1 день составляет: " + Math.round(moneyDay) + "$");
 
-costs = prompt("Введите обязательную статью расходов в этом месяце", "Еда, коммунальные платежи")
-expensive = +prompt("Во сколько обойдется?($)", "50")
+//      Version 2    //
 
+// let money = prompt("Ваш бюджет на месяц?($)", ""),
+//     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+// let appData = {
+//     budget: money,
+//     expenses: {},
+//     optionalExpenses: {},
+//     income: [],
+//     timeData: time,
+//     savings: false
+// };
+// let a1 = prompt("Введите обязательную статью расходов в этом месяце", "Еда, коммунальные платежи"),
+//     a2 = prompt("Во сколько обойдется?($)", "50"),
+//     a3 = prompt("Введите обязательную статью расходов в этом месяце", "Еда, коммунальные платежи"),
+//     a4 = prompt("Во сколько обойдется?($)", "50");
+
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;
+
+// alert(Math.round(appData.budget / 30));
+
+//      Version 3 (cycle for)    //
+
+let money = prompt("Ваш бюджет на месяц?($)", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
-    moneyData: money,
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     timeData: time,
-    expenses: {
-        costs, 
-        expensive    },
-    optionalExpenses: undefined,
-    income: undefined,
     savings: false
 };
 
-let moneyDay =(appData.moneyData - appData.expenses.expensive)/30;
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?($)", "");
 
-let message = alert(" Дата: " + appData.timeData + " С учетом расходов на:" + appData.expenses.costs + " Бюджет на 1 день составляет: " + Math.round(moneyDay) + "$");
+    if ( (typeof(a)) === "string" && typeof(b) === "number" && (typeof(a)) != null
+        && (typeof(b)) != null && a != " " && b != " " && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i = 0;
+    }
+};
+
+alert(Math.round(appData.budget / 30));
